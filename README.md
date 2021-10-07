60b46fb54eadf333be09695d1842534279ae314b
Reggie <hitsupod@gmail.com>
2021-10-06 21:45:53 -0500

    Update Readme & Hardhat

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # epic-nfts
- 
+Mint your own NFT collection and ship a Web3 app to show them off
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'filler',
-      accounts: ['filler'],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
